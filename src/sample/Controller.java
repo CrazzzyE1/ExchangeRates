6fa888e365c9013bfafb2b8da349810161a65431
CrazzzyE1 <litvakivan@gmail.com>
2020-01-17 15:45:10 +0300

    Create start window

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,38 @@
 package sample;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField login_field;
+
+    @FXML
+    private Button authSigInButton;
+
+    @FXML
+    private PasswordField password_field;
+
+    @FXML
+    private Button loginSigInButton;
+
+    @FXML
+    void initialize() {
+        authSigInButton.setOnAction(event -> {
+            System.out.println("START to Login");
+        });
+    }
 }
+
