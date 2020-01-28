1b463feb4ddf56df6f75e354c6dfb9350c99daa6
CrazzzyE1 <litvakivan@gmail.com>
2020-01-29 00:05:35 +0300

    parsing

--- src/sample/controllers/RatesController.java
+++ src/sample/controllers/RatesController.java
@@ -2,11 +2,17 @@ package sample.controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import javafx.fxml.FXML;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class RatesController {
@@ -18,15 +24,43 @@ public class RatesController {
     private URL location;
 
     @FXML
-    private ImageView imageButtonHome;
+    private TextField usdRate;
+
+    @FXML
+    private Label euroRate;
 
     @FXML
     void initialize() throws IOException {
 
-
-//        Document doc = Jsoup.connect("https://www.tinkoff.ru/invest/currencies/USDRUB/").get();
-//        Elements elements = doc.getElementsByAttributeValue("class", "Tooltip__tooltipText_3JY4_");
-//        System.out.println(elements);
+        Timer timer = new Timer();
+        timer.schedule (new SayHello(), 0, 4000); // ставим по расписанию выполнять SayHello каждые 4 секунды
 
     }
+
+    class SayHello extends TimerTask {
+        private String [] str = new String[4];
+        public void run() {
+            Document doc2 = null;
+            try {
+                doc2 = Jsoup.connect("https://www.tinkoff.ru/invest/currencies/").get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Elements elementsRate = doc2.getElementsByAttributeValue("class", "Money__money_1Jk62");
+
+            int i = 0;
+            for (Element rate : elementsRate) {
+                str[i] = rate.text();
+                i++;
+                System.out.print(rate.text());
+            }
+            System.out.println();
+
+            usdRate.setText(str[0] + str[1]);
+            euroRate.setText(str[3]);
+
+        }
+    }
+
+
 }
